23e96194855016d6e1b0a61b7f1ad8307547cfe8
jaem1n207 <roy.jm.lee@gmail.com>
2023-07-13 19:10:12 +0900

    feat: mui

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -3,9 +3,21 @@ import { createRoot } from "react-dom/client";
 import "@pages/popup/index.css";
 import Popup from "@pages/popup/Popup";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 refreshOnUpdate("pages/popup");
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const queryClient = new QueryClient();
 
@@ -16,9 +28,12 @@ function init() {
   }
   const root = createRoot(appContainer);
   root.render(
-    <QueryClientProvider client={queryClient}>
-      <Popup />
-    </QueryClientProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <Popup />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
