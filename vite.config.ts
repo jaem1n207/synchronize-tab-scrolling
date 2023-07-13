4bd326d4c2a08dbc3b88c8bd62697d6446ba9871
jaem1n207 <roy.jm.lee@gmail.com>
2023-07-14 00:43:22 +0900

    fix: Warning with use client directive with Vite & @tanstack/react-query

--- vite.config.ts
+++ vite.config.ts
@@ -46,6 +46,19 @@ export default defineConfig({
     minify: isProduction,
     reportCompressedSize: isProduction,
     rollupOptions: {
+      /**
+       * Ignore "use client" waning since we are not using SSR
+       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+       */
+      onwarn(warning, warn) {
+        if (
+          warning.code === "MODULE_LEVEL_DIRECTIVE" &&
+          warning.message.includes("use client")
+        ) {
+          return;
+        }
+        warn(warning);
+      },
       input: {
         devtools: resolve(pagesDir, "devtools", "index.html"),
         panel: resolve(pagesDir, "panel", "index.html"),
