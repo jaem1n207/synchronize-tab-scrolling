34a8de832a5ecaddab6dda6bf3eef5fb9b7cf139
jaem1n207 <roy.jm.lee@gmail.com>
2023-07-20 11:48:34 +0900

    :white_check_mark: Content jsx가 null을 반환하는지 검사

--- src/pages/content/components/Demo/app.test.tsx
+++ src/pages/content/components/Demo/app.test.tsx
@@ -1,15 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "@pages/content/components/Demo/app";
 
-describe("appTest", () => {
-  test("render text", () => {
-    // given
-    const text = "content view";
-
-    // when
-    render(<App />);
-
-    // then
-    screen.getByText(text);
+describe("contentAppTest", () => {
+  test("should return null", () => {
+    const { container } = render(<App />);
+    expect(container.firstChild).toBeNull();
   });
 });
