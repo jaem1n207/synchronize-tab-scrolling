8bf743ad93adadce7da93ad0d20ca05d08519ea7
jaem1n207 <roy.jm.lee@gmail.com>
2023-08-09 15:54:18 +0900

    :zap: 경량화
    필요없는 코드들 모두 제거

--- src/pages/content/components/Demo/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "@pages/content/components/Demo/app";
-
-describe("contentAppTest", () => {
-  test("should return null", () => {
-    const { container } = render(<App />);
-    expect(container.firstChild).toBeNull();
-  });
-});
--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "@pages/options/Options.css";
+import { t } from "@src/chrome/i18n";
 
 const Options: React.FC = () => {
-  return <div className="container text-lime-400">Options</div>;
+  return (
+    <div className="container text-neutral-600">{t("optionsContent")}</div>
+  );
 };
 
 export default Options;
