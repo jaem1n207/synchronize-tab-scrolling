d46486d0bda0f4f36d7a6d541035af33d239045b
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-19 17:54:19 +0900

    :sparkles: Shadcn ui 컴포넌트 추가 (Button, Dropdown)

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = ({ route }) => {
-	if (route.id === '/') {
-		redirect(302, '/popup');
-	}
-};
