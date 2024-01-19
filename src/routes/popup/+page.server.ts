cd9002cacb48432227883bfe1306364a11a11f7c
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-19 17:54:50 +0900

    :sparkles: 팝업 서버측 작업 추가

--- src/routes/popup/+page.server.ts
+++ src/routes/popup/+page.server.ts
@@ -2,7 +2,17 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	error(402, {
-		message: 'This is a custom error page'
-	});
+	// error(402, {
+	// 	message: 'This is a custom error page'
+	// });
+
+	return {
+		status: 200,
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: {
+			foo: 'bar'
+		}
+	};
 };
