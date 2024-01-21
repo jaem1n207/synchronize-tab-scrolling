772b884e4f0022237f2591cd4cc18b70b213e0cf
이재민 <jaem1n207@naver.com>
2024-01-21 17:13:57 +0900

    :heavy_plus_sign: Static adapter 사용

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from 'sveltekit-adapter-chrome-extension';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,13 +7,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			manifest: 'manifest.json'
-		}),
+		adapter: adapter(),
 		appDir: 'app',
 		alias: {
 			'@/*': './src/*'
