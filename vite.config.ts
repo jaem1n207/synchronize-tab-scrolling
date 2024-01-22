20f608e1d8b499534dd93ffc29d09c87e10a7e0f
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-22 16:14:27 +0900

    :sparkles: 빌드 후 인라인 스크립트 제거 후 따로 추출하는 플러그인 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { readdirSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
 import { join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
 import manifest from './manifest';
 import createManifest from './utils/plugins/create-manifest';
 import watchRebuild from './utils/plugins/watch-rebuild';
+import removeInlineScript from './utils/plugins/remove-inline-script';
 
 // 번들링에서 제외할 파일 경로
 const injectStaticDir = 'src/inject-static';
@@ -17,7 +18,7 @@ const filesPathToExclude = readdirSync(injectStaticDir)
 	.map((filename) => fileURLToPath(new URL(join(injectStaticDir, filename), import.meta.url)));
 
 export default defineConfig({
-	plugins: [sveltekit(), createManifest(manifest), watchRebuild()],
+	plugins: [sveltekit(), createManifest(manifest), removeInlineScript(), watchRebuild()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
