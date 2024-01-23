29dc70e83e1987395ab76e3ea64cf123a93929bb
이재민 <jaem1n207@naver.com>
2024-01-24 00:42:13 +0900

    :bug: Add browser check for getLocalMessage function

--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -1,7 +1,12 @@
+import { browser } from '$app/environment';
 import type EnMessage from '../../static/_locales/en/messages.json';
 import type KoMessage from '../../static/_locales/ko/messages.json';
 
 type MessageKey = keyof typeof EnMessage | keyof typeof KoMessage;
 
-export const getLocalMessage = (messageName: MessageKey): string =>
-	chrome.i18n.getMessage(messageName);
+export const getLocalMessage = (messageName: MessageKey): string => {
+	if (browser) {
+		return chrome.i18n.getMessage(messageName);
+	}
+	return '';
+};
