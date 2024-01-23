e674da56619ae4487df51feffd26bf839447e7b8
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-23 11:31:42 +0900

    :zap: 탭 배열을 순회하는 비용 감소

--- src/lib/tabs/tabs-cmdk.svelte
+++ src/lib/tabs/tabs-cmdk.svelte
@@ -1,111 +1,104 @@
 <script lang="ts">
 	import * as Command from '$lib/components/ui/command';
-	import { createQuery } from '@tanstack/svelte-query';
 
 	import { isEmptyString, isHTMLElement } from '../is';
 	import { kbd } from '../kbd';
-	import { chromeApi, tabKeys } from './tabs';
-	import { cn } from '../utils';
 	import { getUserPreferredTheme } from '../media-query';
+	import { cn } from '../utils';
 
 	// const tabs = createQuery({
 	// 	queryKey: tabKeys.lists(),
 	// 	queryFn: () => chromeApi.getTabs()
 	// });
 
-	let tabs = [
+	let tabs: {
+		id?: number | undefined;
+		title?: string | undefined;
+		favIconUrl?: string | undefined;
+		sessionId?: string | undefined;
+		index: number;
+	}[] = [
 		{
+			index: 0,
 			id: 1,
 			title: 'Google',
 			favIconUrl: 'https://www.google.com/favicon.ico'
 		},
 		{
+			index: 1,
 			id: 2,
 			title:
 				'FacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebookFacebook',
 			favIconUrl: 'https://www.facebook.com/favicon.ico'
 		},
 		{
-			id: 3,
+			index: 2,
+			sessionId: 'ac34adcb',
 			title: 'Twitter',
 			favIconUrl: 'https://www.twitter.com/favicon.ico'
 		},
 		{
+			index: 3,
 			id: 4,
 			title: 'Instagram',
 			favIconUrl: 'https://www.instagram.com/favicon.ico'
 		},
 		{
-			id: 5,
+			index: 4,
+			sessionId: 'svf5324th',
 			title: 'Youtube',
 			favIconUrl: 'https://www.youtube.com/favicon.ico'
 		},
 		{
+			index: 5,
 			id: 6,
 			title: 'Linkedin',
 			favIconUrl: 'https://www.linkedin.com/favicon.ico'
 		},
 		{
+			index: 6,
 			id: 7,
 			title: 'Github',
 			favIconUrl: 'https://www.github.com/favicon.ico'
 		},
 		{
+			index: 7,
 			id: 8,
-			title: 'Stackoverflow',
+			title: 'Stack Overflow',
 			favIconUrl: 'https://www.stackoverflow.com/favicon.ico'
-		},
-		{
-			id: 9,
-			title: 'Medium',
-			favIconUrl: 'https://www.medium.com/favicon.ico'
-		},
-		{
-			id: 10,
-			title: 'Reddit',
-			favIconUrl: 'https://www.reddit.com/favicon.ico'
-		},
-		{
-			id: 11,
-			title: 'Netflix',
-			favIconUrl: 'https://www.netflix.com/favicon.ico'
-		},
-		{
-			id: 12,
-			title: 'Amazon',
-			favIconUrl: 'https://www.amazon.com/favicon.ico'
-		},
-		{
-			id: 13,
-			title: 'Flipkart',
-			favIconUrl: 'https://www.flipkart.com/favicon.ico'
-		},
-		{
-			id: 14,
-			title: 'Snapdeal',
-			favIconUrl: 'https://www.snapdeal.com/favicon.ico'
-		},
-		{
-			id: 15,
-			title: 'Paytm',
-			favIconUrl: 'https://www.paytm.com/favicon.ico'
-		},
-		{
-			id: 16,
-			title: 'Myntra',
-			favIconUrl: 'https://www.myntra.com/favicon.ico'
-		},
-		{
-			id: 17,
-			title: 'Swiggy',
-			favIconUrl: 'https://www.swiggy.com/favicon.ico'
 		}
 	];
 
 	let inputValue: string = '';
 
-	let selectedTabIds: number[] = [];
-	$: isEmptySelectedTabIds = selectedTabIds.length === 0;
+	let selectedTabs = new Map<string, (typeof tabs)[0]>();
+	$: isEmptySelectedTabIds = selectedTabs.size === 0;
+
+	/**
+	 * 선택된 탭을 추가합니다.
+	 *
+	 * 새로운 Map 객체를 생성하고 할당하여 Svelte가 변경을 감지하고 UI를 업데이트하도록 합니다.
+	 *
+	 * @param {chrome.tabs.Tab} tab - 선택할 탭 객체
+	 */
+	const addSelectedTab = (tab: (typeof tabs)[0]) => {
+		const identifier = getTabIdentifier(tab);
+		selectedTabs = new Map(selectedTabs.set(identifier, tab));
+	};
+
+	/**
+	 * 선택된 탭을 제거합니다.
+	 *
+	 * 새로운 Map 객체를 생성하고 할당하여 Svelte가 변경을 감지하고 UI를 업데이트하도록 합니다.
+	 *
+	 * @param {string} identifier - 제거할 탭의 고유 식별자
+	 */
+	const removeSelectedTab = (identifier: string) => {
+		if (selectedTabs.has(identifier)) {
+			selectedTabs = new Map(selectedTabs);
+			selectedTabs.delete(identifier);
+		}
+	};
 
 	const bounce = (node: HTMLElement) => {
 		node.style.transform = 'scale(0.98)';
@@ -114,11 +107,16 @@
 		}, 100);
 	};
 
-	const popSelectedTab = () => {
+	/**
+	 * 마지막으로 선택된 탭을 제거합니다. 선택된 탭이 없으면 아무 작업도 수행하지 않습니다.
+	 */
+	const removeLastSelectedTab = () => {
 		if (isEmptySelectedTabIds) return;
 
-		const next = selectedTabIds[selectedTabIds.length - 1];
-		selectedTabIds = selectedTabIds.filter((id) => id !== next);
+		const lastIdentifier = Array.from(selectedTabs.keys()).pop();
+		if (lastIdentifier) {
+			removeSelectedTab(lastIdentifier);
+		}
 	};
 
 	const handleKeydown = (e: KeyboardEvent) => {
@@ -129,18 +127,33 @@
 
 		if (e.key === kbd.BACKSPACE) {
 			e.preventDefault();
-			popSelectedTab();
+			removeLastSelectedTab();
 			bounce(currentTarget);
 		}
 	};
 
-	const handleTabSelect = (tabId: number) => {
-		{
-			if (selectedTabIds.includes(tabId)) {
-				selectedTabIds = selectedTabIds.filter((id) => id !== tabId);
-			} else {
-				selectedTabIds = [...selectedTabIds, tabId];
-			}
+	/**
+	 * 탭 객체를 받아 고유 식별자를 문자열로 반환합니다. `id`가 있으면 `id`를 반환하고,
+	 * 없으면 `sessionId`를, `sessionId`도 없으면 `index`를 반환합니다.
+	 *
+	 * @param {chrome.tabs.Tab} tab - 사용자가 선택한 탭 객체
+	 */
+	const getTabIdentifier = (tab: (typeof tabs)[0]): string => {
+		return tab.id?.toString() ?? tab.sessionId ?? tab.index.toString();
+	};
+
+	/**
+	 * 사용자가 탭을 클릭할 때 호출됩니다.
+	 *
+	 * @param {chrome.tabs.Tab} tab - 사용자가 선택한 탭 객체
+	 */
+	const handleTabSelect = (tab: (typeof tabs)[0]) => {
+		const identifier = getTabIdentifier(tab);
+
+		if (selectedTabs.has(identifier)) {
+			removeSelectedTab(identifier);
+		} else {
+			addSelectedTab(tab);
 		}
 	};
 
@@ -153,22 +166,20 @@
 
 <Command.Root onKeydown={handleKeydown} class="vercel">
 	<div class="select-none p-1">
-		{#each selectedTabIds as tabId}
-			{#if tabs.find((tab) => tab.id === tabId)}
-				<div
-					class="mx-0.5 mb-1 inline-flex h-6 items-center rounded-sm bg-muted px-2 text-xs font-medium capitalize"
-				>
-					<div class="relative mr-1 flex size-4 shrink-0 overflow-hidden rounded-full">
-						<img
-							class="aspect-square size-full object-cover"
-							src={tabs.find((tab) => tab.id === tabId).favIconUrl}
-							alt=""
-							on:error={handleFaviconError}
-						/>
-					</div>
-					{tabs.find((tab) => tab.id === tabId).title.slice(0, 10)}
+		{#each Array.from(selectedTabs.values()) as tab}
+			<div
+				class="mx-0.5 mb-1 inline-flex h-6 items-center rounded-sm bg-muted px-2 text-xs font-medium capitalize"
+			>
+				<div class="relative mr-1 flex size-4 shrink-0 overflow-hidden rounded-full">
+					<img
+						class="aspect-square size-full object-cover"
+						src={tab.favIconUrl}
+						alt=""
+						on:error={handleFaviconError}
+					/>
 				</div>
-			{/if}
+				{tab.title ? tab.title.slice(0, 10) : '제목 없음'}
+			</div>
 		{/each}
 	</div>
 	<Command.Input placeholder="Type a command or search..." autofocus bind:value={inputValue} />
@@ -177,9 +188,9 @@
 		<Command.Group heading="Tabs">
 			{#each tabs as tab}
 				<Command.Item
-					onSelect={() => handleTabSelect(tab.id)}
+					onSelect={() => handleTabSelect(tab)}
 					class={cn('my-0.5 cursor-pointer py-2 transition-colors aria-selected:bg-muted/50', {
-						'bg-muted': selectedTabIds.includes(tab.id)
+						'bg-muted': selectedTabs.has(getTabIdentifier(tab))
 					})}
 				>
 					<div class="relative mr-2 flex size-6 shrink-0 overflow-hidden rounded-full">
