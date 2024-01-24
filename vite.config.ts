5c39b7cdbec3a602e3dbd69d954f970c92736c9c
이재민 <jaem1n207@naver.com>
2024-01-25 01:31:12 +0900

    :sparkles: 빌드 결과물에서 인라인 스크립트 추출 후 스크립트 파일 삽입

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vitest/config';
 import manifest from './manifest';
 import createManifest from './utils/plugins/create-manifest';
 import watchRebuild from './utils/plugins/watch-rebuild';
-import removeInlineScript from './utils/plugins/remove-inline-script';
+import extractInlineScript from './utils/plugins/extract-inline-script';
 
 // 번들링에서 제외할 파일 경로
 const injectStaticDir = 'src/inject-static';
@@ -18,7 +18,7 @@ const filesPathToExclude = readdirSync(injectStaticDir)
 	.map((filename) => fileURLToPath(new URL(join(injectStaticDir, filename), import.meta.url)));
 
 export default defineConfig({
-	plugins: [sveltekit(), createManifest(manifest), removeInlineScript(), watchRebuild()],
+	plugins: [sveltekit(), createManifest(manifest), extractInlineScript(), watchRebuild()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
