1d2b0c5015843e9be03a7f96cbc1b53561613e4f
이재민 <jaem1n207@naver.com>
2024-01-25 01:41:45 +0900

    :sparkles: Add watch files for supported languages

--- utils/plugins/watch-rebuild.ts
+++ utils/plugins/watch-rebuild.ts
@@ -1,10 +1,14 @@
 import { dirname, resolve } from 'path';
 import type { PluginOption } from 'vite';
 
+const SUPPORTED_LANGUAGES = ['en', 'ko'] as const;
+
 const __dirname = dirname(new URL(import.meta.url).pathname);
 const rootDir = resolve(__dirname, '..', '..');
 const manifestFile = resolve(rootDir, 'manifest.ts');
 const viteConfigFile = resolve(rootDir, 'vite.config.ts');
+const localeDir = resolve(rootDir, 'static', '_locales');
+const messagesFile = (locale: string) => resolve(localeDir, locale, 'messages.json');
 
 const watchRebuild = (): PluginOption => {
 	return {
@@ -12,6 +16,9 @@ const watchRebuild = (): PluginOption => {
 		async buildStart() {
 			this.addWatchFile(manifestFile);
 			this.addWatchFile(viteConfigFile);
+			for (const locale of SUPPORTED_LANGUAGES) {
+				this.addWatchFile(messagesFile(locale));
+			}
 		}
 	};
 };
