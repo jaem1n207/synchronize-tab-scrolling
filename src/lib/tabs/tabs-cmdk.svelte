72cc1793bffd41d38aa882be5fafa23f5c09a03b
이재민 <jaem1n207@naver.com>
2024-01-28 02:19:19 +0900

    :bug: Allow keyboard navigation even when text input is not focused
    https://github.com/pacocoursey/cmdk/pull/61

--- src/lib/tabs/tabs-cmdk.svelte
+++ src/lib/tabs/tabs-cmdk.svelte
@@ -68,8 +68,9 @@
 
 <Command.Root
 	onKeydown={handleKeydown}
+	tabindex={-1}
 	class={cn(
-		'relative p-0.5',
+		'relative p-0.5 outline-none',
 		isSyncing &&
 			'border-2 border-solid border-transparent before:absolute before:inset-0 before:animate-clippath before:content-normal before:rounded-md before:border-2 before:border-solid before:border-neutral-700 dark:before:border-[#a8efff99]'
 	)}
