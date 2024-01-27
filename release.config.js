76bf96efcbbbdbabd2b5c7361a1ee5f6a23cdd18
이재민 <jaem1n207@naver.com>
2024-01-28 04:21:39 +0900

    fix: Safari keyword to package.json

--- release.config.js
+++ release.config.js
@@ -7,12 +7,6 @@ export default {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/npm',
-		[
-			'@semantic-release/git',
-			{
-				assets: 'package.json',
-				message: 'chore(release): ${nextRelease.version} [skip ci]\\n\\n${nextRelease.notes}'
-			}
-		]
+		'@semantic-release/git'
 	]
 };
