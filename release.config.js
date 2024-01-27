c41e828a4dee78fa8b25e2b868a37ec7f428e82e
이재민 <jaem1n207@naver.com>
2024-01-28 04:13:50 +0900

    :construction_worker: Add semantic-release plugins to release.config.js

--- release.config.js
+++ release.config.js
@@ -4,6 +4,9 @@
 export default {
 	branches: ['main'],
 	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
 		'@semantic-release/git',
 		{
 			assets: 'package.json',
