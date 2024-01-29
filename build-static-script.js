f3b8aaeb23fb80b243698af32119fbcbfbe54903
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-29 15:16:51 +0900

    fix: 스크립트를 빌드하기 전에 파일이 존재하는지 확인
    fix: 기본 빌드 옵션 제거하고 역할 명확히

--- build-static-script.js
+++ build-static-script.js
@@ -1,36 +1,41 @@
 // TODO: If it is supported to integrate Vite and Bun's native bundler settings,
 // remove this file and make it a vite plugin so that it can be integrated into the vite settings.
-import { build } from 'bun';
+import * as Bun from 'bun';
 
 import colorLog from './utils/log';
 
-(async () => {
-	/**
-	 * @type {import('bun').BuildConfig}
-	 */
-	const defaultBuildOptions = {
-		format: 'esm',
-		target: 'browser',
-		minify: true,
-		outdir: './static'
-	};
+/**
+ *
+ * @param {string} filePath
+ * @param {import('bun').BuildConfig['naming']} naming
+ */
+const buildScript = async (filePath, naming) => {
+  const file = Bun.file(filePath);
+  const isFileExists = await file.exists();
+  if (!isFileExists) {
+    colorLog(`❌ File not found: ${filePath}`, 'error', true);
+    process.exit(1);
+  }
 
-	try {
-		await Promise.all([
-			build({
-				...defaultBuildOptions,
-				entrypoints: ['./src/inject-static/content-script.inject.ts'],
-				naming: 'content-script.js'
-			}),
-			build({
-				...defaultBuildOptions,
-				entrypoints: ['./src/inject-static/background-script.inject.ts'],
-				naming: 'background-script.js'
-			})
-		]);
-		colorLog('✅ Injection script build success', 'success', true);
-	} catch (error) {
-		colorLog(`Injection script build failed: ${error}`, 'error', true);
-		process.exit(1);
-	}
+  try {
+    await Bun.build({
+      format: 'esm',
+      target: 'browser',
+      minify: true,
+      outdir: './static',
+      entrypoints: [filePath],
+      naming
+    });
+    colorLog(`✅ ${naming} build success`, 'success', true);
+  } catch (error) {
+    colorLog(`❌ ${naming} build failed: ${error}`, 'error', true);
+    process.exit(1);
+  }
+};
+
+(async () => {
+  await Promise.all([
+    buildScript('./src/inject-static/content-script.inject.ts', 'content-script.js'),
+    buildScript('./src/inject-static/background-script.inject.ts', 'background-script.js')
+  ]);
 })();
