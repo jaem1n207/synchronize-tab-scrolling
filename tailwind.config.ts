643c72fa37591b07f68b4b7b6adeda9bbf75b46e
jaem1n207 <roy.jm.lee@gmail.com>
2024-01-30 14:24:12 +0900

    :lipstick: Add tertiary color variables and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,10 @@ const config = {
           DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
           foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
         },
+        tertiary: {
+          DEFAULT: 'hsl(var(--tertiary) / <alpha-value>)',
+          foreground: 'hsl(var(--tertiary-foreground) / <alpha-value>)'
+        },
         destructive: {
           DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
           foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
