b7f71a4f9b1c52026ed40ec92b07f41d566fbc40
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-06 18:05:48 +0900

    feat: platform detection logic

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -1,8 +1,5 @@
 // https://github.com/darkreader/darkreader/blob/main/src/utils/platform.ts
 
-declare const __CHROMIUM_MV3__: boolean;
-declare const __FIREFOX_MV2__: boolean;
-
 interface UserAgentData {
   brands: Array<{
     brand: string;
@@ -33,38 +30,22 @@ const platform = isNavigatorDefined
     : navigator.platform.toLowerCase()
   : 'some platform';
 
-export const isChromium =
-  __CHROMIUM_MV3__ ||
-  (!__FIREFOX_MV2__ && (userAgent.includes('chrome') || userAgent.includes('chromium')));
+export const isChromium = userAgent.includes('chrome') || userAgent.includes('chromium');
 export const isFirefox =
-  __FIREFOX_MV2__ ||
-  (!__CHROMIUM_MV3__ &&
-    (userAgent.includes('firefox') ||
-      userAgent.includes('thunderbird') ||
-      userAgent.includes('librewolf')));
-export const isVivaldi = __CHROMIUM_MV3__ && !__FIREFOX_MV2__ && userAgent.includes('vivaldi');
-export const isYaBrowser = __CHROMIUM_MV3__ && !__FIREFOX_MV2__ && userAgent.includes('yabrowser');
-export const isOpera =
-  __CHROMIUM_MV3__ &&
-  !__FIREFOX_MV2__ &&
-  (userAgent.includes('opr') || userAgent.includes('opera'));
-export const isEdge = __CHROMIUM_MV3__ && !__FIREFOX_MV2__ && userAgent.includes('edg');
-export const isSafari =
-  !__CHROMIUM_MV3__ && !__FIREFOX_MV2__ && userAgent.includes('safari') && !isChromium;
+  userAgent.includes('firefox') ||
+  userAgent.includes('thunderbird') ||
+  userAgent.includes('librewolf');
+export const isVivaldi = isChromium && userAgent.includes('vivaldi');
+export const isYaBrowser = isChromium && userAgent.includes('yabrowser');
+export const isOpera = isChromium && (userAgent.includes('opr') || userAgent.includes('opera'));
+export const isEdge = isChromium && userAgent.includes('edg');
+export const isSafari = !isChromium && userAgent.includes('safari');
 export const isWindows = platform.startsWith('win');
 export const isMacOS = platform.startsWith('mac');
 export const isMobile =
   isNavigatorDefined && navigator.userAgentData
     ? navigator.userAgentData.mobile
     : userAgent.includes('mobile');
-
-export const chromiumVersion = (() => {
-  const m = userAgent.match(/chrom(?:e|ium)(?:\/| )([^ ]+)/);
-  if (m && m[1]) {
-    return m[1];
-  }
-  return '';
-})();
 
 export const firefoxVersion = (() => {
   const m = userAgent.match(/(?:firefox|librewolf)(?:\/| )([^ ]+)/);
