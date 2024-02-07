0ddcd87f193d7e577db9a031f6525db57326aac7
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-07 13:21:33 +0900

    feat: 브라우저별 manifest 구성 파일 생성

--- manifest.ts
+++ manifest.ts
@@ -1,22 +1,20 @@
 import packageJson from './package.json';
 
-/**
- * After changing, please reload the extension at `chrome://extensions`
- */
-const manifest: chrome.runtime.ManifestV3 = {
-  manifest_version: 3,
+const manifest: browser._manifest.WebExtensionManifest = {
+  manifest_version: 2,
   name: '__MSG_extName__',
   default_locale: 'en',
   version: packageJson.version,
-  permissions: ['scripting', 'storage', 'tabs'],
-  host_permissions: ['*://*/*'],
-  action: {
+  permissions: ['scripting', 'storage', 'tabs', '*://*/*'],
+  content_security_policy: "script-src 'self' 'unsafe-eval'; object-src 'self'",
+  browser_action: {
     default_title: '__MSG_extName__',
     default_icon: {
       '16': 'icons/logo-16.png',
       '32': 'icons/logo-32.png',
       '48': 'icons/logo-48.png',
-      '72': 'icons/logo-72.png'
+      '72': 'icons/logo-72.png',
+      '128': 'icons/logo-128.png'
     },
     default_popup: 'index.html'
   },
@@ -28,8 +26,8 @@ const manifest: chrome.runtime.ManifestV3 = {
     '512': 'icons/logo-512.png'
   },
   background: {
-    service_worker: 'background-script.js',
-    type: 'module'
+    scripts: ['background-script.js'],
+    persistent: false
   },
   content_scripts: [
     {
---
+++ utils/plugins/create-manifests.ts
@@ -0,0 +1,86 @@
+import { resolve } from 'node:path';
+import type { PluginOption } from 'vite';
+
+import colorLog from '../log';
+import { PLATFORM, getBuildDir } from '../paths';
+import { writeJSON } from '../utils';
+
+/**
+ * Chrome or Mozilla manifest file
+ */
+type Manifest = chrome.runtime.ManifestV3 | browser._manifest.WebExtensionManifest;
+
+const validateManifest = (manifest: Manifest) => {
+  if (!manifest.name) {
+    throw new Error('Manifest must have a name');
+  }
+  if (!manifest.version) {
+    throw new Error('Manifest must have a version');
+  }
+};
+
+const patchManifest = async ({ debug, platform }: { debug: boolean; platform: PLATFORM }) => {
+  const manifest = await import('../../manifest').then((m) => m.default);
+  const patched: Manifest = { ...manifest };
+
+  if (platform === 'chrome-mv3') {
+    patched.manifest_version = 3;
+    patched.minimum_chrome_version = '88';
+    patched.permissions = ['scripting', 'storage', 'tabs'];
+    patched.host_permissions = ['*://*/*'];
+    patched.background = {
+      service_worker: 'background-script.js',
+      type: 'module'
+    };
+    patched.action = {
+      default_title: '__MSG_extName__',
+      default_icon: {
+        '16': 'icons/logo-16.png',
+        '32': 'icons/logo-32.png',
+        '48': 'icons/logo-48.png',
+        '72': 'icons/logo-72.png',
+        '128': 'icons/logo-128.png'
+      },
+      default_popup: 'index.html'
+    };
+    patched.browser_action = undefined;
+  }
+  if (platform === 'firefox') {
+    patched.browser_specific_settings = {
+      gecko: {
+        id: 'addon@synchronize-tab-scrolling.org',
+        strict_min_version: '57.0'
+      }
+    };
+  }
+  if (debug) {
+    patched.name = `(Debug) Synchronize Tab Scrolling`;
+    patched.version = '1';
+  }
+
+  validateManifest(patched);
+
+  return patched;
+};
+
+const createManifests = async ({
+  debug,
+  platforms
+}: {
+  debug: boolean;
+  platforms: PLATFORM[];
+}): Promise<PluginOption> => {
+  return {
+    name: 'create-manifests',
+    async closeBundle() {
+      for (const platform of platforms) {
+        const manifest = await patchManifest({ debug, platform });
+        const buildDir = getBuildDir({ debug, platform });
+        await writeJSON(resolve(buildDir, 'manifest.json'), manifest);
+      }
+      colorLog(`📦 Created manifests for ${platforms.join(', ')}`, 'success', true);
+    }
+  };
+};
+
+export default createManifests;
