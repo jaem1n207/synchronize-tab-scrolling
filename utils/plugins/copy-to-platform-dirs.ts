3330444a13360b42a74828b3b0067d3aa0ce93e3
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-13 17:35:13 +0900

    fix: 파일 복사할 때 이미지 파일 손상되지 않도록 수정

--- utils/plugins/copy-to-platform-dirs.ts
+++ utils/plugins/copy-to-platform-dirs.ts
@@ -1,10 +1,42 @@
 import { globby } from 'globby';
-import fs from 'node:fs/promises';
+import { copyFile as _copyFile, readdir } from 'node:fs/promises';
 import path from 'node:path';
 import type { PluginOption } from 'vite';
 
-import { PLATFORM, getBuildDir, rootPath } from '../paths';
-import { mkDirIfMissing, readFile, writeFile } from '../utils';
+import { PLATFORM, getDestDir, rootPath } from '../paths';
+import { measureTime, mkDirIfMissing, removeFolder } from '../utils';
+
+const buildDir = 'build';
+
+const getPaths = async (patterns: string | string[]) => {
+  return await globby(patterns);
+};
+
+const copyFile = async (src: string, dest: string) => {
+  await mkDirIfMissing(dest);
+  await _copyFile(src, dest);
+};
+
+const copyEntries = [
+  '_locales/**/messages.json',
+  'app/**/*.*',
+  'icons/**/*.png',
+  '*.js',
+  'index.html'
+];
+
+const getCwdPath = (path: string) => path.substring(buildDir.length + 1);
+
+const copyEntry = async (
+  path: string,
+  { debug, platform }: { debug: boolean; platform: PLATFORM }
+) => {
+  const cwdPath = getCwdPath(path);
+  const destDir = getDestDir({ debug, platform });
+  const src = `${buildDir}/${cwdPath}`;
+  const dest = `${destDir}/${cwdPath}`;
+  await copyFile(src, dest);
+};
 
 const copyToPlatformDirsPlugin = async ({
   debug,
@@ -15,36 +47,25 @@ const copyToPlatformDirsPlugin = async ({
   platforms: PLATFORM[];
   delay?: number;
 }): Promise<PluginOption> => {
-  const copyFilesToPlatformDir = async (platform: PLATFORM) => {
-    const buildDir = rootPath('build');
-    const targetDir = getBuildDir({ debug, platform });
-
-    // Define patterns for all files and folders except the 'build/release' folder
-    const patterns = [`${buildDir}/**/*`, `!${buildDir}/release/**/*`];
-
-    const paths = await globby(patterns);
-
-    for (const srcPath of paths) {
-      const relativePath = path.relative(buildDir, srcPath);
-      const destPath = path.join(targetDir, relativePath);
-
-      // For files, destPath is the file path, so to get the folder path, use '...' to get the folder path.
-      await mkDirIfMissing(path.join(destPath, '..'));
-
-      const data = await readFile(srcPath);
-      await writeFile(destPath, data);
-    }
+  const copy = async () => {
+    const promises = copyEntries.flatMap(async (entry) => {
+      const files = await getPaths(`${buildDir}/${entry}`);
+      return files.flatMap((file) =>
+        platforms.map((platform) => copyEntry(file, { debug, platform }))
+      );
+    });
+    await Promise.all(promises);
   };
 
   const cleanBuildFolder = async () => {
     const buildDir = rootPath('build');
-    const items = await fs.readdir(buildDir);
+    const items = await readdir(buildDir);
 
     for (const item of items) {
       // Remove all folders except the 'release' and 'debug' folders
       if (item !== 'release' && item !== 'debug') {
         const itemPath = path.join(buildDir, item);
-        await fs.rm(itemPath, { recursive: true, force: true });
+        await removeFolder(itemPath);
       }
     }
   };
@@ -58,15 +79,13 @@ const copyToPlatformDirsPlugin = async ({
        * Unfortunately there's no hook to make it run after the `closeBundle` cycle, so wait one second before running it.
        */
       await Bun.sleep(delay);
-      const promises: Promise<void>[] = [];
-      for (const platform of platforms) {
-        promises.push(copyFilesToPlatformDir(platform));
-      }
-      await Promise.all(promises);
+
+      await measureTime(
+        copy(),
+        '📦 Copied build files to platform directories, excluding the release folder'
+      );
 
       await cleanBuildFolder();
-
-      console.log(' 📦 Copied build files to platform directories, excluding the release folder');
     }
   };
 };
