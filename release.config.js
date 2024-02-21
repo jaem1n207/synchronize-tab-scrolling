6759ea48fcc605d065dff59f4ce1382e9077c7a6
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-21 16:25:01 +0900

    ci: update release workflow and dependencies

--- release.config.js
+++ release.config.js
@@ -8,6 +8,7 @@ export default {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
+    '@semantic-release/github',
     '@semantic-release/git'
   ]
 };
