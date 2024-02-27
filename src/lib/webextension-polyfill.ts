796e75b864e7e0c5c7ba5779fbeb44c3d331ea5a
jaem1n207 <roy.jm.lee@gmail.com>
2024-02-27 17:07:36 +0900

    refactor: chrome 객체에 존재하지 않는 속성에 대한 오류 처리 수정

--- src/lib/webextension-polyfill.ts
+++ src/lib/webextension-polyfill.ts
@@ -2,7 +2,7 @@
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const wrapAsyncFunction = (method: FunctionShape) => {
-  return (target: typeof chrome | typeof browser, ...args: any[]) => {
+  return (target: typeof chrome, ...args: any[]) => {
     return new Promise((resolve, reject) => {
       method.call(target, ...args, (result: any) => {
         if (chrome.runtime.lastError) {
@@ -15,11 +15,7 @@ const wrapAsyncFunction = (method: FunctionShape) => {
   };
 };
 
-const wrapMethod = (
-  target: typeof chrome | typeof browser,
-  method: any,
-  wrapper: FunctionShape
-) => {
+const wrapMethod = (target: typeof chrome, method: any, wrapper: FunctionShape) => {
   return new Proxy(method, {
     apply(_, thisArg, args) {
       return wrapper.call(thisArg, target, ...args);
@@ -27,27 +23,49 @@ const wrapMethod = (
   });
 };
 
-const wrapObject = (target: typeof chrome | typeof browser) => {
+const wrapObject = (target: typeof chrome) => {
   const cache = Object.create(null);
-  const handlers: ProxyHandler<typeof chrome | typeof browser> = {
+  const handlers: ProxyHandler<typeof chrome> = {
     has(_, prop) {
       return prop in target || prop in cache;
     },
     get(_, prop, receiver) {
-      if (typeof prop !== 'string' || !(prop in target)) {
-        throw new Error(`Property "${String(prop)}" does not exist on the chrome object.`);
-      }
-
       if (prop in cache) {
         return cache[prop];
+      }
+
+      if (!(prop in target)) {
+        console.error(`Property "${String(prop)}" does not exist on the chrome object.`);
+        return undefined;
       }
 
       if (typeof target[prop as keyof typeof target] === 'function') {
         cache[prop] = wrapMethod(target, target[prop as keyof typeof target], wrapAsyncFunction);
         return cache[prop];
+      } else {
+        Object.defineProperty(cache, prop, {
+          configurable: true,
+          enumerable: true,
+          get() {
+            return target[prop as keyof typeof target];
+          },
+          set(value) {
+            target[prop as keyof typeof target] = value;
+          }
+        });
       }
 
-      return Reflect.get(target, prop, receiver);
+      return Reflect.get(cache, prop, receiver);
+    },
+    set(_, prop, value) {
+      prop in cache ? (cache[prop] = value) : (target[prop as keyof typeof target] = value);
+      return true;
+    },
+    defineProperty(_, prop, descriptor) {
+      return Reflect.defineProperty(cache, prop, descriptor);
+    },
+    deleteProperty(_, prop) {
+      return Reflect.deleteProperty(cache, prop);
     }
   };
 
