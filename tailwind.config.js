2ca099e53baa36042a74caf9f369974811e4d807
jaem1n207 <tech.jmtt@gmail.com>
2025-12-08 11:01:56 +0900

    chore: add empty tailwind.config.js for shadcn cli compatibility
    this placeholder file is required by shadcn cli to function properly.
    actual styling is handled by unocss (uno.config.ts) with unocss-preset-shadcn.
    
    see: https://github.com/unocss-community/unocss-preset-shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// This is a placeholder file for shadcn CLI compatibility.
+// Actual styling is handled by UnoCSS (uno.config.ts).
+// See: https://github.com/unocss-community/unocss-preset-shadcn
+export default {};
+
