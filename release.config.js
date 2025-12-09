5ad925efd4a052b6f575ff1cb70a53e571932477
jaem1n207 <tech.jmtt@gmail.com>
2025-12-09 18:37:52 +0900

    chore: update GitHub release comments configuration

--- release.config.js
+++ release.config.js
@@ -8,7 +8,14 @@ export default {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
-    '@semantic-release/github',
+    [
+      '@semantic-release/github',
+      {
+        // PR/이슈에 릴리스 코멘트 추가 비활성화
+        successComment: false,
+        failComment: false,
+      },
+    ],
     '@semantic-release/git',
   ],
 };
