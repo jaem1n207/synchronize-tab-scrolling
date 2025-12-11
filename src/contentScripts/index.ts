1531dd8215785dd3d3432a2578e881cc8fc1cf04
jaem1n207 <tech.jmtt@gmail.com>
2025-12-11 18:08:07 +0900

    chore: remove sentry from content script

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,13 +1,8 @@
-import { initializeSentry } from '~/shared/lib/sentry_init';
-
 import '~/shared/styles';
 import { initScrollSync } from './scrollSync';
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
-  // Sentry 초기화
-  initializeSentry();
-
   // Initialize scroll synchronization system
   initScrollSync();
 
